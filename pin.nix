45ee9a447e0481a788d4b79a2cb62acadaf7cca5
Jappie Klooster <jappieklooster@hotmail.com>
2021-09-24 19:27:25 +0200

    I've no idea waht nixpkgs did, but I want none of it
    Revert "Upgrade pin"
    
    This reverts commit 1dba21fa6ff03cffc184615acbc74ff128f3a358.

--- pin.nix
+++ pin.nix
@@ -2,10 +2,12 @@ let
 pinnedPkgs = 
     (builtins.fetchGit {
     # Descriptive name to make the store path easier to identify
-    name = "nixos-pin-2021.08.27";
-    url = "https://github.com/nixos/nixpkgs/";
-    rev = "21c937f8cb1e6adcfeb36dfd6c90d9d9bfab1d28";
+    name = "nixos-pin-19.10.2019";
+    url = https://github.com/nixos/nixpkgs/;
+    rev = "f203d50d09052792866f926e7c2d3b3bf76d3388";
     }) ;
 in
 import pinnedPkgs {
+    # since I also use this for clients I don't want to have to care
+    config.allowUnfree = true; # took me too long to figure out
 }
