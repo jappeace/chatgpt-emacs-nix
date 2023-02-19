8b449ac2789411fbf24ac942e5ae0200cd8cda2c
Jappie Klooster <jappieklooster@hotmail.com>
2023-02-19 15:25:39 +0100

    add to src

--- flake.nix
+++ flake.nix
@@ -5,10 +5,10 @@
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
   };
 
-  outputs = { self, nixpkgs }: {
+  outputs = { self, nixpkgs }:
     let
       pkgs = nixpkgs.legacyPackages.x86_64-linux;
-    in
-    defaultPackage.x86_64-linux = pkgs.emacsPackages.callPackage ./chatgpt.el {};
+    in {
+    defaultPackage.x86_64-linux = pkgs.emacsPackages.callPackage ./src {};
   };
 }
